095e8ddef3ca3e4d89102d4ec04c32c6424cce43
Jules <jgmz.jg@gmail.com>
2017-03-22 14:41:45 -0500

    Finish homework exercises

--- Homework2/js-minicamp-homework-2-master/exercises.js
+++ Homework2/js-minicamp-homework-2-master/exercises.js
@@ -47,7 +47,11 @@ function isInRange(num) {
 }
 
 function isInteger(num) {
-  
+  if (Number.isInteger(num) === true) {
+    return true;
+  } else {
+    return false;
+  }
   //return true if num is an integer
   //0.8 -> false
   //1 -> true
@@ -57,6 +61,15 @@ function isInteger(num) {
 }
 
 function fizzBuzz(num) {
+  if (num % 3 === 0) {
+    return 'fizz';
+  } if (num % 5 === 0) {
+    return 'buzz';
+  } if (num % 3 === 0 && num % 5 === 0){
+    return 'fizzbuzz';
+  } else {
+    return num;
+  }
   //if num is divisible by 3 return 'fizz'
   //if num is divisible by 5 return 'buzz'
   //if num is divisible by 3 & 5 return 'fizzbuzz'
@@ -64,6 +77,11 @@ function fizzBuzz(num) {
 }
 
 function isPrime(num) {
+  for (var i = 2; 1 < num; i++){
+    if (num % i === 0){
+      return false;
+    }
+  } return true;
   //return true if num is prime.
   //otherwise return false
   //hint: a prime number is only evenly divisible by itself and 1
@@ -72,35 +90,47 @@ function isPrime(num) {
 }
 
 function returnFirst(arr) {
+  return arr[0];
   //return the first item from the array
 }
 
 function returnLast(arr) {
+  return arr.length - 1;
   //return the last item of the array
 }
 
 function getArrayLength(arr) {
+  return arr.length;
   //return the length of the array
 }
 
 function incrementByOne(arr) {
+  var newArray = arr.map(function(num){
+    num++;
+  });
+  return newArray;
   //arr is an array of integers  
   //increase each integer by one
   //return the array
 }
 
 function addItemToArray(arr, item) {
+  var new = arr.push(item);
+  return new;
   //add the item to the end of the array
   //return the array
 }
 
 function addItemToFront(arr, item) {
+  ver new = arr.unshift(item);
   //add the item to the front of the array
   //return the array
   //hint: use the array method .unshift
 }
 
 function wordsToSentence(words) {
+  var sentence = words.join(' ');
+  return sentence;
   //words is an array of strings
   //return a string that is all of the words concatenated together
   //spaces need to be between each word
@@ -108,21 +138,36 @@ function wordsToSentence(words) {
 }
 
 function contains(arr, item) {
+  if (arr.includes(item) === true){
+    return true;
+  } else {
+    return false;
+  }
   //check to see if item is inside of arr
   //return true if it is, otherwise return false
 }
 
 function addNumbers(numbers) {
+  var sum = numbers.reduce(function(a,b){
+    return a+b;
+  }, 0);
   //numbers is an array of integers.
   //add all of the integers and return the value
 }
 
 function averageTestScore(testScores) {
+  var sum = testScores.reduce(function(a,b){
+    return a+b;
+  }, 0);
+  var average = sum/testScores.length;
+  return average;
   //testScores is an array.  Iterate over testScores and compute the average.
   //return the average
 }
 
 function largestNumber(numbers) {
+  var biggest = Math.max(numbers);
+  return biggest;
   //numbers is an array of integers
   //return the largest integer
 }
